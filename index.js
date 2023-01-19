4dcc5db0473c96d860b564618db26eab0a137143
Kevin Duggan <kevinduggan95@gmail.com>
2023-01-19 16:26:59 -0600

    arrays lab 9q's

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+//cats.unshift('Milo', 'Otis', 'Garfield')
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+//destructivelyAppendCat('Maria Jose')
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+
+function appendCat(name) {
+    const cats2 = [...cats, name];
+    return cats2
+}
+function prependCat(name) {
+    const cats3 = [name,...cats];
+    return cats3
+}
+function removeLastCat() {
+    const cats4 = cats.slice();
+    cats4.pop()
+    return cats4
+
+}
+function removeFirstCat() {
+    const cats5 = cats.slice(1)
+    return cats5
+}
